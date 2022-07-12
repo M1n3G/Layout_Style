762c0e136d63fc73139ff16fc8a501d0decf051a
ChingYJ <chingyj-wm19@student.tarc.edu.my>
2022-07-12 15:56:35 +0800

    Commit 20020712 1558 - add Dice Img - Extract styles

--- app/src/main/java/com/example/my2pmhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/my2pmhelloworld/MainActivity.kt
@@ -2,24 +2,44 @@ package com.example.my2pmhelloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImg :ImageView
+    lateinit var numberText: TextView
+    lateinit var nameText: TextView
+    lateinit var playerName: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        numberText = findViewById(R.id.numberText)
+        diceImg = findViewById(R.id.diceImage)
+        nameText = findViewById(R.id.enterName)
+        playerName = findViewById(R.id.playerName)
         val rollButton : Button = findViewById(R.id.rollButton)
         rollButton.setOnClickListener { rollDice() }
+        val updateBtn : Button = findViewById(R.id.updateButton)
+        updateBtn.setOnClickListener { updateName(it) }
+    }
+
+    private fun updateName(view: View){
+        playerName.text = nameText.text
     }
 
     private fun rollDice(){
         Toast.makeText(this, "ROLL", Toast.LENGTH_SHORT).show()
 
         val randomNumber = (1..6).random()
-        val numberText : TextView = findViewById(R.id.numberText)
         numberText.text = randomNumber.toString()
+        val imgSrc = when (randomNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImg.setImageResource(imgSrc)
     }
 }
